796e5e3464d162ee0d02b0ba7a29bb2583f08a2f
icedilo45 <icedilo45@gmail.com>
2024-02-24 06:29:00 +0000

    First Commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is AppCare Service App.Powered by AppCare Solutions</Text>
+      <Text>Nana Korankye</Text>
       <StatusBar style="auto" />
     </View>
   );
